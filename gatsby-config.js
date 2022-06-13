dc3bbd12d5bf64d7995d935482f9996b757c3b50
pramodmorthala07 <pramod.morthala@winfosolutions.com>
2022-06-13 17:23:39 +0530

    update
    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,27 +13,24 @@ module.exports = {
         id: process.env.GTAG,
       },
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          process.env.GA4,
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: process.env.GTAG,
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+
+    //     trackingIds: [
+    //       process.env.GA4,
+    //     ],
+
+    //     gtagConfig: {
+    //       optimize_id: process.env.GTAG,
+    //       anonymize_ip: true,
+    //     },
+
+    //     pluginConfig: {
+    //       head: true,
+    //     },
+    //   },
+    // },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
   ],
